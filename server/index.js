67b8f6f34cb9b7484841beb33fc3236af0ed6d66
NeoZ666 <neoz.blockchain@gmail.com>
2023-06-30 15:48:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
+const merkleTree = new MerkleTree(niceList);
 // TODO: hardcode a merkle root here representing the whole nice list
+
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
